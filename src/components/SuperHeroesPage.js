8ab26a3d75a01e13cd26c40d0e852c6f68496e26
Luis Costa <runtime@Runtimes-MBP.Home>
2022-04-21 15:58:27 +0100

    Initial Project Setup

---
+++ src/components/SuperHeroesPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Container } from '@mui/material'
+
+export default function SuperHeroesPage() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/superheroes')
+      .then((res) => {
+        setData(res.data)
+        setIsLoading(false)
+      })
+  }, [])
+
+  if(isLoading) {
+    return <h2>Loading...</h2>
+  }
+  return (
+    <Container>
+      <h2>
+        Super Heroes Page
+      </h2>
+      {data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </Container>
+  )
+}
