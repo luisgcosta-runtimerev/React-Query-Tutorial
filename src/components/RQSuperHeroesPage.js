36d779806980778f40376cdb5ae57cc032a58677
Luis Costa <runtime@Runtimes-MBP.Home>
2022-04-21 18:20:19 +0100

    Refetch on click

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import axios from 'axios';
-import { Container, Typography } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
 
 const fetchSuperHeroes = () => axios.get('http://localhost:8000/superheroes');
 
 export default function RQSuperHeroesPage() {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
@@ -14,14 +14,15 @@ export default function RQSuperHeroesPage() {
       // staleTime: 30000, // set stale time to 30 seconds
       // refetchOnMount: true, // only fecth data if the data is stale ** if it is set to 'always', data is always fetched
       // refetchOnWindowFocus: true, // refectch data when tab loses focus and gain focus again - only fecth data if the data is stale ** if it is set to 'always', data is always fetched
-      refetchInterval: 2000, // automatically refetch data after 2 seconds - it's paused if window isn't focus
-      refetchIntervalInBackground: true // refetch if tab isn't focus
+      // refetchInterval: 2000, // automatically refetch data after 2 seconds - it's paused if window isn't focus
+      // refetchIntervalInBackground: true, // refetch if tab isn't focus
+      enabled: false
     }
   );
 
   console.log({ isLoading, isFetching });
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return (
       <Container>
         <Typography>Loading...</Typography>
@@ -38,6 +39,7 @@ export default function RQSuperHeroesPage() {
 
   return (
     <Container>
+      <Button onClick={refetch}>Fecth Heroes</Button>
       {data?.data.map((hero) => (
         <Typography key={hero.name}>{hero.name}</Typography>
       ))}
